56b4f104dfe42e11d877ea6ee8dc03b58e84b595
M-HT <M-HT@users.noreply.github.com>
2017-12-09 17:45:57 +0100

    Fix for filesystems with 64-bit inodes

--- games/X-Com - Terror from the Deep/SR-Tactical/input/pandora.c
+++ games/X-Com - Terror from the Deep/SR-Tactical/input/pandora.c
@@ -1,6 +1,6 @@
 /**
  *
- *  Copyright (C) 2016 Roman Pauer
+ *  Copyright (C) 2016-2017 Roman Pauer
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy of
  *  this software and associated documentation files (the "Software"), to deal in
@@ -22,6 +22,7 @@
  *
  */
 
+#define _FILE_OFFSET_BITS 64
 #include "../Game_defs.h"
 #include "../Game_vars.h"
 #include "../Tactical-proc-events.h"
--- midi-plugins/midi-bassmidi.c
+++ midi-plugins/midi-bassmidi.c
@@ -1,6 +1,6 @@
 /**
  *
- *  Copyright (C) 2016 Roman Pauer
+ *  Copyright (C) 2016-2017 Roman Pauer
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy of
  *  this software and associated documentation files (the "Software"), to deal in
@@ -27,6 +27,7 @@
 
     #include <windows.h>
 #else
+    #define _FILE_OFFSET_BITS 64
     #include <unistd.h>
     #include <sys/types.h>
     #include <dirent.h>

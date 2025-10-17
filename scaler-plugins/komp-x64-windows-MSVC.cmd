f1406fa8d47e0541982c95a6658e0326171b3451
M-HT <M-HT@users.noreply.github.com>
2025-10-17 23:36:48 +0200

    scaler-plugins: add compiler parameter

--- scaler-plugins/komp-x64-windows-MSVC.cmd
+++ scaler-plugins/komp-x64-windows-MSVC.cmd
@@ -7,7 +7,7 @@ link /NOLOGO /DLL /OUT:scaler-hqx.dll scaler-hqx.obj hqx/*.obj /DEF:scaler.def /
 
 cl /nologo /c /O2 /W3 /wd4305 /MD /EHsc scaler-xbrz.cpp
 cd xbrz
-cl /nologo /c /O2 /W3 /wd4305 /wd4244 /MD /arch:SSE2 /DNO_BUFFER_HEAP_ALLOCATION /DNO_EXTRA_SCALERS /DNO_ALPHA_SUPPORT xbrz.cpp
+cl /nologo /c /O2 /W3 /wd4305 /wd4244 /MD /arch:SSE2 /fp:fast /DNO_BUFFER_HEAP_ALLOCATION /DNO_EXTRA_SCALERS /DNO_ALPHA_SUPPORT xbrz.cpp
 cd ..
 link /NOLOGO /DLL /OUT:scaler-xbrz.dll scaler-xbrz.obj xbrz/*.obj /DEF:scaler.def /MACHINE:X64
 
